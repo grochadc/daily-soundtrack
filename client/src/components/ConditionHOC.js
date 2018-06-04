1b9047747ff49220497168bcbac39de85a2c53b8
Gonzalo Rocha <grochadc@gmail.com>
2018-06-04 14:56:06 -0500

    Delete unnecessary components.

--- client/src/components/ConditionHOC.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ConditionHOC(Component, props){
-  if(props.condition){
-    return <Component {... props}/>;
-  } else {
-    return null;
-  }
-}
