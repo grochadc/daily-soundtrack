ee9326c93dd684504f46886879cc97e6e3b11278
Gonzalo Rocha <grochadc@gmail.com>
2018-06-03 21:34:27 -0500

    Restify endpoint

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 const api = require('./routes/api');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const restify = require('express-restify-mongoose');
+const router = express.Router()
+
+const trackSchema = require('./schemas/track');
 
 app.use(bodyParser.json());
+app.use(methodOverride());
 
-app.use('/api', api);
+mongoose.connect('mongodb://localhost/test');
 
-app.get('/', (req, res) =>{
-  res.send('Home');
-})
+restify.serve(router, mongoose.model('Track', trackSchema ));
 
+app.use(router);
 
 app.listen(3030, function () {
   console.log('Example app listening on port 3030!');
