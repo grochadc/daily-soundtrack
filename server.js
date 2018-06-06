2014ae8e1f784d14671799d7e83921ca605c7c47
Gonzalo Rocha <grochadc@gmail.com>
2018-06-05 19:22:05 -0500

    Add an endpoint to retirve Spotify API token

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 const api = require('./routes/api');
@@ -6,6 +7,8 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const restify = require('express-restify-mongoose');
 const router = express.Router()
+
+const axios = require('axios');
 
 const trackSchema = require('./schemas/track');
 
@@ -22,8 +25,25 @@ mongoose.connect('mongodb://localhost/test');
 
 restify.serve(router, mongoose.model('Track', trackSchema ));
 
+
+let secret = process.env.SECRET;
+console.log(secret ? 'Secret is good': 'No secret provided');
+router.get('/token', (req, res) =>{
+  axios({
+    method: 'post',
+    headers: {
+      'Authorization': 'Basic '+secret
+    },
+    url: 'https://accounts.spotify.com/api/token',
+    data: 'grant_type=client_credentials'
+  })
+  .then((response) => res.send(response.data))
+  .catch((err) => res.send(err));
+});
+
 app.use(router);
 
-app.listen(3030, function () {
-  console.log('Example app listening on port 3030!');
+let port = process.env.PORT || 3030
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
 });
