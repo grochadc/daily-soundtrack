ce5e80f68df7ba0c24582cb96b2a3718d014c67f
Gonzalo Rocha <grochadc@gmail.com>
2018-06-06 18:10:26 -0500

    Add endpoint to get the previous and next songs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 var express = require('express');
 var app = express();
 const api = require('./routes/api');
@@ -11,9 +12,11 @@ const router = express.Router()
 const axios = require('axios');
 
 const trackSchema = require('./schemas/track');
+const trackModel = mongoose.model('Track', trackSchema );
 
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(express.static(path.resolve(__dirname, 'client/build/')));
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -23,12 +26,21 @@ router.use(function(req, res, next) {
 
 mongoose.connect('mongodb://localhost/test');
 
-restify.serve(router, mongoose.model('Track', trackSchema ));
+restify.serve(router, trackModel);
 
+router.get('/api/v1/Track/around/:id',(req, res) => {
+  async function findTracks(){
+    let beforeDocs = await trackModel.find({'_id':{'$lt':req.params.id}}).sort({'_id':-1}).limit(1)
+    let afterDocs = await trackModel.find({'_id':{'$gt':req.params.id}}).sort('_id').limit(1)
+
+    res.send([beforeDocs.length ? beforeDocs : null , afterDocs.length ? afterDocs : null]);
+  }
+  findTracks();
+});
 
 let secret = process.env.SECRET;
 console.log(secret ? 'Secret is good': 'No secret provided');
-router.get('/token', (req, res) =>{
+router.get('/token', (req, res) => {
   axios({
     method: 'post',
     headers: {
