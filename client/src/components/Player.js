8d9ebba61f9afa4f6da8190ffa2016a61bdc66bf
Gonzalo Rocha <grochadc@gmail.com>
2018-06-06 18:09:47 -0500

    Add grid to player

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Loading  from './Loading';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 
 export default class Player extends Component {
   constructor(){
     super();
     this.state = {
-      track: null
+      track: null,
+      trackBefore: null,
+      trackAfter: null
     }
   }
   componentDidMount(){
@@ -28,15 +31,36 @@ export default class Player extends Component {
     .catch(function (error) {
       console.log(error);
     });
+
+    axios.get('http://localhost:3030/api/v1/Track/around/'+this.props.id)
+    .then((response) => {
+      let docs = response.data;
+      that.setState({
+        trackBefore: docs[0][0],
+        trackAfter: docs[1][0]
+      })
+    })
+    .catch((err) => console.log(err));
   }
   render(){
     let URL = 'https://open.spotify.com/embed/track/'+this.state.track;
     return(
-      <div className='player'>
+
+      <Grid>
+        <Row>
+          <Col sm={7} smOffset={2}>
+                {this.state.trackBefore ? <Button><a href={'/player/'+this.state.trackBefore._id}>Before</a></Button> : null}
+                {this.state.trackAfter ? <Button className="pull-right"><a href={'/player/'+this.state.trackAfter._id}>After</a></Button> : null}
+          </Col>
+      </Row>
+      <Row>
+        <Col sm={5} smOffset={3}>
         {this.state.track===null ?
           <Loading /> :
-          <div align="center"><iframe src={URL} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media" title='currentTrack'></iframe></div>}
-      </div>
+          <iframe src={URL} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media" title='currentTrack'></iframe>}
+        </Col>
+      </Row>
+      </Grid>
     )
   }
 }
