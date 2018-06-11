5a5ef54daddefd3f74f20d5707637bba5e827502
Gonzalo Rocha <grochadc@gmail.com>
2018-06-11 09:25:35 -0500

    Fix timeDiff fn where it treated >10 hours as a week

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,19 +4,19 @@ function timeDiff(then){
   let timeDiff = now - then
   let Seconds = (timeDiff/1000);
 
-  if(Seconds>2336000) {
-    return [Math.floor(Seconds/2336000), 'year']
-  } else if(Seconds>198400) {
-    return [Math.floor(Seconds/198400), 'month']
-  } else if(Seconds>44800) {
-    return [Math.floor(Seconds/44800), 'week']
-  } else if(Seconds>64000) {
-    return [Math.floor(Seconds/64000), 'day']
-  } else if(Seconds>3600){
-    return [Math.floor(Seconds/3600), 'hour']
-  } else if(Seconds>60){
-    return [Math.floor(Seconds/60), 'minute']
+  if(Seconds>31540000) {
+      return [Math.floor(Seconds/31540000), 'year']
+    } else if(Seconds>2628000) {
+      return [Math.floor(Seconds/2628000), 'month']
+    } else if(Seconds>604800) {
+      return [Math.floor(Seconds/604800), 'week']
+    } else if(Seconds>86400) {
+      return [Math.floor(Seconds/86400), 'day']
+    } else if(Seconds>3600){
+      return [Math.floor(Seconds/3600), 'hour']
+    } else if(Seconds>60){
+      return [Math.floor(Seconds/60), 'minute']
+    }
   }
-}
 
 export default timeDiff;
