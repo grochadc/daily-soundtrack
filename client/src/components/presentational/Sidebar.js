6f0a01293b2ce8701fcea3637f369cc188b72a84
Gonzalo Rocha <grochadc@gmail.com>
2018-06-11 09:25:10 -0500

    Add display name to Navbar

--- client/src/components/presentational/Sidebar.js
+++ client/src/components/presentational/Sidebar.js
@@ -3,6 +3,7 @@ import { Link } from '@reach/router';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 function Sidebar(props){
+  let loggedIn = props.user_info!=null;
   return (
     <Navbar fixedTop>
       <Navbar.Header>
@@ -11,11 +12,11 @@ function Sidebar(props){
         </Navbar.Brand>
       </Navbar.Header>
     <Nav bsStyle="pills">
-      { props.user_info!=null ?
+      { loggedIn ?
         <Nav>
-          <NavItem>
-            <Link to='/profile'>Profile</Link>
-          </NavItem>
+        <NavItem>
+          <Link to={'/playlist/'+props.user_info.id}>My Tracks</Link>
+        </NavItem>
         <NavItem>
           <Link to='/add'>Add Track</Link>
         </NavItem>
@@ -23,6 +24,7 @@ function Sidebar(props){
         <NavItem href='http://localhost:3030/login'>Login</NavItem>
       }
     </Nav>
+    { loggedIn ? <Nav  pullRight><NavItem><Link to={'/profile'}>{props.user_info.display_name}</Link></NavItem></Nav> : null }
   </Navbar>
   )
 };
