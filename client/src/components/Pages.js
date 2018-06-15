bcb55ef410b0699b8c69842c3fe4f80b3360417e
Gonzalo Rocha <grochadc@gmail.com>
2018-06-15 10:17:14 -0500

    Add page header with current user

--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Playlist from "./Playlist";
+import { PageHeader } from "react-bootstrap";
 
 function Home(props) {
   return <Playlist />;
 }
 
 function UserTracks(props) {
-  return <Playlist user={props.user} />;
+  return (
+    <div>
+      <PageHeader>{props.user}'s Playlist</PageHeader>
+      <Playlist user={props.user} />
+    </div>
+  );
 }
 
 export { Home, UserTracks };
