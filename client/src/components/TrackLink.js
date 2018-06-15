01b6de9c50dd9788b27e7461a0e74a7f46b8a4b4
Gonzalo Rocha <grochadc@gmail.com>
2018-06-15 10:14:25 -0500

    Factor/ Change component TrackLink to use the props from {...props}

--- client/src/components/TrackLink.js
+++ client/src/components/TrackLink.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { Link } from '@reach/router';
-import { Panel, Media } from 'react-bootstrap';
-import { distanceInWordsToNow } from 'date-fns';
+import React from "react";
+import { Link } from "@reach/router";
+import { Panel, Media } from "react-bootstrap";
+import { distanceInWordsToNow } from "date-fns";
 
-function TrackLink(props){
-  let { info } = props
+function TrackLink(props) {
+  let { track_info } = props;
+  console.log(props);
 
-  return(
-      <Panel>
-        <Panel.Body>
-          <Link to={'/player/'+props.id} style={{ textDecoration: 'none'}}>
+  return (
+    <Panel>
+      <Panel.Body>
+        <Link to={"/player/" + props._id} style={{ textDecoration: "none" }}>
           <Media>
-              <Media.Left><img alt={''} src={info.art} width="100" height="100"/></Media.Left>
-              <Media.Body>
-              <h3>{info.title}</h3>
-              by {info.artist}
+            <Media.Left>
+              <img alt={""} src={track_info.art} width="100" height="100" />
+            </Media.Left>
+            <Media.Body>
+              <h3>{track_info.title}</h3>
+              by {track_info.artist}
             </Media.Body>
           </Media>
-          </Link>
-        </Panel.Body>
-        <Panel.Footer>
-          Posted by: <Link to={'/playlist/'+props.user}>{props.user}</Link> <div className='small'>{distanceInWordsToNow(new Date(props.timestamp))} ago</div><br />
-        </Panel.Footer>
-      </Panel>
-  )
+        </Link>
+      </Panel.Body>
+      <Panel.Footer>
+        Posted by: <Link to={"/playlist/" + props.user}>{props.user}</Link>{" "}
+        <div className="small">
+          {distanceInWordsToNow(new Date(props.date))} ago
+        </div>
+        <br />
+      </Panel.Footer>
+    </Panel>
+  );
 }
 
 export default TrackLink;
