b70dad65ea69824ef092ccba168b6a3b2f44e559
Gonzalo Rocha <grochadc@gmail.com>
2018-06-17 12:32:00 -0500

    Add a utility to convert Object to URL query

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,22 +1,30 @@
-function timeDiff(then){
-  let now = new Date().getTime()
+function timeDiff(then) {
+  let now = new Date().getTime();
 
-  let timeDiff = now - then
-  let Seconds = (timeDiff/1000);
+  let timeDiff = now - then;
+  let Seconds = timeDiff / 1000;
 
-  if(Seconds>31540000) {
-      return [Math.floor(Seconds/31540000), 'year']
-    } else if(Seconds>2628000) {
-      return [Math.floor(Seconds/2628000), 'month']
-    } else if(Seconds>604800) {
-      return [Math.floor(Seconds/604800), 'week']
-    } else if(Seconds>86400) {
-      return [Math.floor(Seconds/86400), 'day']
-    } else if(Seconds>3600){
-      return [Math.floor(Seconds/3600), 'hour']
-    } else if(Seconds>60){
-      return [Math.floor(Seconds/60), 'minute']
-    }
+  if (Seconds > 31540000) {
+    return [Math.floor(Seconds / 31540000), "year"];
+  } else if (Seconds > 2628000) {
+    return [Math.floor(Seconds / 2628000), "month"];
+  } else if (Seconds > 604800) {
+    return [Math.floor(Seconds / 604800), "week"];
+  } else if (Seconds > 86400) {
+    return [Math.floor(Seconds / 86400), "day"];
+  } else if (Seconds > 3600) {
+    return [Math.floor(Seconds / 3600), "hour"];
+  } else if (Seconds > 60) {
+    return [Math.floor(Seconds / 60), "minute"];
   }
+}
 
-export default timeDiff;
+const objectToURL = obj =>
+  Object.entries(obj)
+    .map(([key, val]) => {
+      let newVal = typeof val === "object" ? JSON.stringify(val) : val;
+      return `${key}=${newVal}`;
+    })
+    .join("&");
+
+export { timeDiff, objectToURL };
