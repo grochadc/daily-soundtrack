8b701c4282e4adfa31cc7f6ef283886455c36e3e
Gonzalo Rocha <grochadc@gmail.com>
2018-07-02 14:57:45 -0500

    fixed typo on from

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { verify } from "jsonwebtoken";
 import reducer from "./redux/Reducer";
 import App from "./ConnectedApp";
 import registerServiceWorker from "./registerServiceWorker";
 
 const initialState = {
-  user: null
+  user: null,
+  spotify_info: {
+    id: null
+  },
+  loggedIn: false
+};
+
+const fetchUserMiddleware = store => next => action => {
+  if (action.type === "FETCH_USER") {
+    let query = {
+      _id: verify(action.payload, process.env.REACT_APP_WEBTOKEN_SECRET).userid
+    };
+  }
 };
 
 const store = createStore(
